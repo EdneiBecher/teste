52784761f6d9ed9445cab89e68d4c8da3ea03943
Ednei Becher <edneibecher@gmail.com>
2020-08-23 23:39:39 -0300

    Calculando média, mediana e desvio por especie e por bloco. Usando tidyverse e R base.

--- script_mudas.R
+++ script_mudas.R
@@ -10,5 +10,23 @@ str(mudas)
 # ajustando o tiipo de variável para factor
 mudas$especie<- as.factor(mudas$especie)
 
-# calculando a media por especie
+# calculando a media, mediana e desvio pd por especie
 tapply(mudas$altura, mudas$especie, mean)
+tapply(mudas$altura, mudas$especie, median)
+tapply(mudas$altura, mudas$especie, sd)
+
+# calculando media, mediana e desvio pd por especie e por bloco
+
+mudas$bloco<- as.factor(mudas$bloco)
+
+library(dplyr)
+
+mudas %>%
+  group_by(especie, bloco) %>% 
+  summarise(media=mean(altura), mediana=median(altura), desvio=sd(altura))
+
+# fazendo o mesmo com tapply
+tapply(mudas$altura, list(mudas$especie, mudas$bloco), mean)
+tapply(mudas$altura, list(mudas$especie, mudas$bloco), median)
+tapply(mudas$altura, list(mudas$especie, mudas$bloco), sd)
+
